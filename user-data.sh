de556435cf3fa32414e367cdc03ceec1e88d4c5c
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-01 02:40:44 +0700

    ci_cd.yml

--- user-data.sh
+++ user-data.sh
@@ -11,5 +11,8 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 
 
-sudo docker pull fieldrock/cs360_frontend_image_test:latest
-docker run -d -p 3000:3000 --name cs360_frontend_container fieldrock/cs360_frontend_image_test:latest
+sudo docker pull korakrit/cs360_frontend_image_test:latest
+docker run -d -p 3000:3000 --name cs360_frontend_container korakrit/cs360_frontend_image_test:latest
+
+sudo docker pull korakrit/cs360_backtend_image_test:latest
+docker run -d -p 1337:1337 --name cs360_backtend_container korakrit/cs360_backtend_image_test:latest
