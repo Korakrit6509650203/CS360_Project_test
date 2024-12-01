9966a536c95f92aef5b9b3df4476df82f85108c6
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-01 13:24:08 +0700

    fix cicd.yml

--- user-data.sh
+++ user-data.sh
@@ -9,10 +9,14 @@ sudo systemctl enable docker
 
 # Add the ec2-user to the Docker group (so you can run Docker commands without sudo)
 sudo usermod -aG docker ec2-user
+newgrp docker
 
+echo "Starting Docker pull and run..."
 
 sudo docker pull korakrit/cs360_frontend_image_test:latest
 docker run -p 3000:3000 --name cs360_frontend_container korakrit/cs360_frontend_image_test:latest
 
 sudo docker pull korakrit/cs360_backtend_image_test:latest
 docker run -p 1337:1337 --name cs360_backtend_container korakrit/cs360_backtend_image_test:latest
+
+echo "Docker containers are running."
