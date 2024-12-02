69ac11378836c2d3f088ea6abc05c8a9b8cc34f7
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-02 18:01:37 +0700

    fix cicd

--- user-backend.sh
+++
@@ -1,6 +0,0 @@
-echo "Starting Docker pull and run..."
-
-sudo docker pull korakrit/cs360_backend_image_test:latest
-sudo docker run -p 1337:1337 --name cs360_backend_container korakrit/cs360_backend_image_test:latest
-
-echo "Docker containers are running."
