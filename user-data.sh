307816982e5930079cfa02074adac5e7292e1e20
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-08 15:31:59 +0700

    fix

--- user-data.sh
+++ user-data.sh
@@ -11,19 +11,23 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 newgrp docker
 
+echo "Docker name: {{DOCKER_USENAME}}"
+echo "Instance ID: {{INSTANCE_ID}}"
+
+
 #"Starting Docker pull and run..."
 
-sudo docker pull ${{ secrets.DOCKER_USERNAME }} /cs360_frontend_image_test:latest
-sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${{ secrets.DOCKER_USERNAME }}/cs360_frontend_image_test:latest
+sudo docker pull {{DOCKER_USENAME}}/cs360_frontend_image_test:latest
+sudo docker run -d -p 3000:3000 --name cs360_frontend_container {{DOCKER_USENAME}}/cs360_frontend_image_test:latest
 sudo docker exec cs360_frontend_container sh -c "
 PUBLIC_IP=\$(curl -s ifconfig.me) &&
 echo \"REACT_APP_STRIPE_APP_KEY=\$(openssl rand -hex 32)\" > /usr/src/app/.env &&
-echo \"REACT_APP_DEV_URL=http://\$PUBLIC_IP:1337\" >> /usr/src/app/.env &&
+echo \"REACT_APP_DEV_URL=http://{{INSTANCE_ID}}:1337\" >> /usr/src/app/.env &&
 echo \"REACT_APP_STRIPE_PUBLISHABLE_KEY=\$(openssl rand -hex 32)\" >> /usr/src/app/.env
 echo "Exiting script..."
 exit 0"
 sudo docker exec cs360_frontend_container npm run build
 
-sudo docker pull ${{ secrets.DOCKER_USERNAME }}/cs360_backend_image_test:latest
-sudo docker run -p 1337:1337 --name cs360_backend_container ${{ secrets.DOCKER_USERNAME }}/cs360_backend_image_test:latest
+sudo docker pull {{DOCKER_USENAME}}/cs360_backend_image_test:latest
+sudo docker run -p 1337:1337 --name cs360_backend_container {{DOCKER_USENAME}}/cs360_backend_image_test:latest
 #"Docker containers are running."
